0eb3b46dc37b974a478e854ec80d79a4ad4bde71
bear-zd <a827569457@163.com>
2025-04-14 18:34:51 +0800

    local test best

--- utils.py
+++ utils.py
@@ -1,38 +1,57 @@
-import sklearn
 import numpy as np
+from sklearn.cluster import KMeans
+from scipy.optimize import linear_sum_assignment
+import sklearn.preprocessing as preprocessing
 
-def cosine_similarity(x, y):
-    if y.shape[0] != 1:
-        sim = [np.dot(x, y[j]) / (np.linalg.norm(x) * np.linalg.norm(y[j])) for j in range(y.shape[0])]
-        return sum(sim) / len(sim)
-    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+def cosine_distance(x: np.array, y: np.array):
+    return  - np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
 
-def euclidean_distance(x, y):
-    if y.shape[0] != 1:
-        dis = [np.linalg.norm(x - y[j]) for j in range(y.shape[0])]
-        return sum(dis) / len(dis)
+def euclidean_distance(x: np.array, y: np.array):
     return np.linalg.norm(x - y)
 
 def distance_based(measure="cosine"):
-    def distance_attack(collect_data, known_data, known_label):
+    def distance_attack(collect_data: np.array, known_data: np.array, known_label: dict):
+        collect_data = preprocessing.normalize(collect_data)
+        known_data = preprocessing.normalize(known_data)
         if measure == "cosine":
-            similarity = cosine_similarity
+            similarity = cosine_distance
         else:
             similarity = euclidean_distance
         collect_label = []
         for i in range(collect_data.shape[0]):
             sim = [similarity(collect_data[i], known_data[j]) for j in range(known_data.shape[0])]
-            collect_label.append(known_label[np.argmax(sim)])
+            collect_label.append(known_label[np.argmin(sim)])
         return np.array(collect_label)
     return distance_attack
 
-def kmeans_based(collect_data, known_data, known_label):
-    from sklearn.cluster import KMeans
-    kmeans = KMeans(n_clusters=len(np.unique(known_label)), random_state=0).fit(collect_data)
-    collect_label = kmeans.predict(known_data)
-    pred_idx = kmeans.predict(collect_data)
-    pred_label = [known_label[collect_label[i]] for i in pred_idx]
-    return np.array(pred_label)
+def kmeans_based(collect_data: np.array, known_data: np.array, known_labels: dict):
+
+    unique_labels = np.unique(known_labels)
+    n_clusters = len(unique_labels)
+    
+    known_dict = {}
+    for label in unique_labels:
+        known_dict[label] = known_data[known_labels == label]
+    
+    init_centroids = []
+    for label in unique_labels:
+        centroid = known_dict[label][np.random.choice(len(known_dict[label]))]
+        init_centroids.append(centroid)
+
+    kmeans = KMeans(n_clusters=n_clusters, init=np.array(init_centroids), n_init=1)
+    kmeans.fit(collect_data)
+
+    cluster_labels = kmeans.labels_
+    known_cluster_labels = kmeans.predict(known_data)
+    confusion_matrix = np.zeros((n_clusters, n_clusters), dtype=np.int64)
+    for i in range(len(known_labels)):
+        true_label_idx = np.where(unique_labels == known_labels[i])[0][0]
+        pred_label_idx = known_cluster_labels[i]
+        confusion_matrix[true_label_idx, pred_label_idx] += 1
+    row_ind, col_ind = linear_sum_assignment(confusion_matrix.max() - confusion_matrix)
+    cluster_to_label_map = {col_ind[i]: unique_labels[row_ind[i]] for i in range(len(row_ind))}
+    pred_labels = np.array([cluster_to_label_map.get(label, -1) for label in cluster_labels])
+    return pred_labels
 
 def random_known_data(known_data, known_label, num):
     known_data_list = []
@@ -45,3 +64,13 @@ def random_known_data(known_data, known_label, num):
     known_data = np.concatenate(known_data_list, axis=0)
     known_label = np.concatenate(known_label_list, axis=0)
     return known_data, known_label
+
+# if __name__ == "__main__":
+#     data = np.load("test.npy")
+#     labels = np.load("test_l.npy")
+#     data = preprocessing.normalize(data)
+
+#     known_data, known_label = random_known_data(data, labels, 40)
+#     func = kmeans_based
+#     res = func(data, known_data, known_label)
+#     print(sum(res == labels))
